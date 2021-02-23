2b8ddbadbbe9e5e78772d22f6e2a1e7f90597ba1
Kokoserver <owonikokoolaluwa@gmail.com>
2021-02-23 02:42:53 +0100

    changing

--- khairo/settings.py
+++ khairo/settings.py
@@ -5,5 +5,5 @@ load_dotenv(dotenv_path="../.env")
 template = Jinja2Templates(directory="./khairo/template").TemplateResponse
 
 API_WEBSITE_URL =  "https://khairodiet.herokuapp.com/api/v1" #'http://127.0.0.1:8000/api/v1'
-WEBSITE_URL = "https://khairoview.herokuapp.com" #'http://127.0.0.1:3000'
+WEBSITE_URL = "https://khairodietview.herokuapp.com" #'http://127.0.0.1:3000'
 DEBUG = False
