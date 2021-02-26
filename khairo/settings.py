f80d17bfd1ba9e8f77f363bfc5c1c23518a50a6f
Kokoserver <owonikokoolaluwa@gmail.com>
2021-02-26 13:30:32 +0100

    making changes to settings

--- khairo/settings.py
+++ khairo/settings.py
@@ -4,9 +4,6 @@ load_dotenv(dotenv_path="../.env")
 import os
 template = Jinja2Templates(directory="./khairo/template").TemplateResponse
 DEBUG = os.getenv("DEBUG")
-
-WEBSITE_URL = "https://khairodietview.herokuapp.com"
-API_WEBSITE_URL =   "https://khairodiet.herokuapp.com/api/v1"
-# WEBSITE_URL = 'http://127.0.0.1:3000'
-# API_WEBSITE_URL =   'http://127.0.0.1:8000/api/v1'
+WEBSITE_URL = os.getenv("WEBSITE_URL")
+API_WEBSITE_URL = os.getenv("API_WEBSITE_URL")
 
